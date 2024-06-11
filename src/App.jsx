a6f320c4488bafd9eaa5a50897911b25b8d0fc07
Jess247 <jesss8692@hotmail.com>
2024-06-11 10:43:01 +0200

    Add current time and display onscreen

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState, useEffect } from 'react'
 
 
 function App() {
+  const time = new Date()
   const [bg, setBg] = useState("https://images.unsplash.com/photo-1715589600919-c0f1b626879d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2MTk0NzR8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTgwMTg3NDJ8&ixlib=rb-4.0.3&q=80&w=1080") 
   const [weather, setWeather] = useState({})
-
-
+  const [currentTime, setCurrentTime] = useState(time.toLocaleString('de-DE', {hour: '2-digit', minute: '2-digit'}))
+  const regionNames = new Intl.DisplayNames(['en'], {type: 'region'})
+  
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => 
       fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
@@ -18,8 +20,9 @@ function App() {
         })
         .then(data => setWeather(
           {
-            temp:Math.round(data.main.temp), 
+            temp: Math.round(data.main.temp), 
             city: data.name, 
+            country: regionNames.of(data.sys.country),
             icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
             description: data.weather[0].description
           }))
@@ -41,17 +44,14 @@ function App() {
   },[])
 
   useEffect(() => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${location.lat}&long=${location.long}&units=metric`)
-      .then(res => {
-        if(res.ok) {
-          return res.json()
-        } else {
-          return Promise.reject(res.status)
-        }
-      })
-      .then(data => setWeather(data))
-      .catch(err => console.log(`The following error occurred fetching the weather: ${err}`))
-  }, [location])
+    const interval = setInterval(() => {
+      setCurrentTime(time.toLocaleString('de-DE', {hour: '2-digit', minute: '2-digit'}))
+    }, 1000)
+
+    return () => clearInterval(interval)
+  },[time])
+
+  
 
 
   return (
@@ -68,15 +68,15 @@ function App() {
           </div>
         </div>
         <div className='flex flex-col items-center gap-1 text-shadow'>
-          <time className='text-9xl font-bold'>
-            <span id='hour'>1</span>:<span id='minutes'>00</span>
+          <time className='text-8xl font-bold'>
+            <span>{currentTime}</span>
           </time>
           <p className='text-lg font-medium'>
             I am constantly growing and evolving into a better person
           </p>
         </div>
         <div className='flex justify-between text-shadow'>
-          <p>Bremen, Germany</p>
+          <p>{weather.city}, {weather.country}</p>
           <button>todo</button>
           <div className='absolute right-8 bottom-20 opacity-0'>
             <p>Todo</p>
